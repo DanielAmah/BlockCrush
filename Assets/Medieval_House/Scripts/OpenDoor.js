f9c0f85a5fd0d323d18cbb4ecaad98f0e832f19c
jattoabdul <jattoade@gmail.com>
2017-11-18 09:11:54 +0100

    added end point house and side blockade

---
+++ Assets/Medieval_House/Scripts/OpenDoor.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+var doorObject:GameObject;
+var openedDoor:boolean;
+function Start () {
+	openedDoor=true;
+}
+
+function Update () {
+	
+}
+
+//function OnTriggerEnter(other:Collider){
+//	if (openedDoor){
+//		doorObject.GetComponent.<Animation>().Play();
+//		openedDoor=false;
+//		Debug.Log("open");
+//	}
+//}
+
+function OnTriggerStay(other:Collider){
+	if (openedDoor){
+		doorObject.GetComponent.<Animation>().Play();
+		openedDoor=false;
+		Debug.Log("open");
+	}
+}
