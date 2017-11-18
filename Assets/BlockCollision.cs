409edd602c4a5c8b5939897cbddf0ebf0b4e564e
DanielAmah <amahdanieljack@gmail.com>
2017-11-18 15:18:28 +0100

    add score to game screen

--- Assets/BlockCollision.cs
+++ Assets/BlockCollision.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 
 public class BlockCollision : MonoBehaviour {
@@ -8,12 +9,16 @@ public class BlockCollision : MonoBehaviour {
 	public int life;
 	public String obTag;
 	public int points;
-
+	public Text score;
+	public String scoreText;
 	// Use this for initialization
 	void Start () {
 		obTag = "unknown";
 		life = 5;
 		points = 0;
+		scoreText = "Score: ";
+		setScoreText ();
+		// score.text = "Score: " + points.toString ();
 	}
 	
 	// Update is called once per frame
@@ -34,8 +39,12 @@ public class BlockCollision : MonoBehaviour {
 	{
 		if (other.gameObject.tag == "coins") {
 			points += 10;
+			setScoreText ();
+			// score.text = "Count: " + points.ToString ();
 			Destroy (other.gameObject);
 		}
 	}
-		
+	void setScoreText() {
+		score.text = scoreText + points.ToString ();
+	}
 }
