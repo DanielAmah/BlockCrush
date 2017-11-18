bb6d54801c4bf5f4a5b1a44a020062f3e2d16ef0
DanielAmah <amahdanieljack@gmail.com>
2017-11-18 11:28:28 +0100

    add life

---
+++ Assets/BlockCollision.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BlockCollision : MonoBehaviour {
+
+	public int life;
+	public String obTag;
+	public int points;
+
+	// Use this for initialization
+	void Start () {
+		obTag = "unknown";
+		life = 5;
+		points = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision col){
+		obTag = col.gameObject.tag;
+		if (col.gameObject.tag == "fireObstacle"){
+			life-=1;
+
+		}
+		if (col.gameObject.tag == "lander") {
+			points += 10;
+		}
+
+	}
+
+//	void OnCollisionEnter(Collision col){
+//		obTag = col.gameObject.tag;
+//		if (col.gameObject.tag == "lander") {
+//			points += 10;
+//		}
+//	}
+}
